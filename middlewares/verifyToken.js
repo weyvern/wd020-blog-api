02c604472e9de3e0c917b16bcd0d733f483a2b40
Jorge Paul <jserranopaul@gmail.com>
2021-12-07 16:15:12 +0100

    Verify token async

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -1,21 +1,18 @@
 import jwt from 'jsonwebtoken';
+import asyncHandler from './asycnHandler.js';
 import ErrorResponse from '../utils/ErrorResponse.js';
 import User from '../models/User.js';
 
-const verifyToken = async (req, res, next) => {
-  try {
-    const {
-      headers: { authorization }
-    } = req;
-    if (!authorization) throw new ErrorResponse('Please log in', 401);
-    const { _id } = jwt.verify(authorization, process.env.JWT_SECRET);
-    const user = await User.findById(_id);
-    if (!user) throw new ErrorResponse('User does not exist', 404);
-    req.user = user;
-    next();
-  } catch (error) {
-    next(error);
-  }
-};
+const verifyToken = asyncHandler(async (req, res, next) => {
+  const {
+    headers: { authorization }
+  } = req;
+  if (!authorization) throw new ErrorResponse('Please log in', 401);
+  const { _id } = jwt.verify(authorization, process.env.JWT_SECRET);
+  const user = await User.findById(_id);
+  if (!user) throw new ErrorResponse('User does not exist', 404);
+  req.user = user;
+  next();
+});
 
 export default verifyToken;
